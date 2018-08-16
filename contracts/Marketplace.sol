a57bb210c2fc5e014425c7e45fbaabf43c7f91db
Xi Lin <xilin6@gmail.com>
2018-08-16 12:54:18 -0700

    update marketplace contract for inital iteration of actions

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -12,21 +12,36 @@ contract MarketPlace {
     mapping (address => uint8) public role;
 
     // store owner storage
-    Storefront[] public Storefronts;
+    Storefront[] public storefronts;
+    uint storefrontCount;
 
     struct Storefront {
-        uint storeId;
+        uint id; //also used as index in storefronts array
         address owner;
         uint balance;
+        string name;
+        bool isActive;
         Product[] products;
     }
 
     struct Product {
         uint productId;
+        uint storefrontId;
         string name;
         uint price;
         uint quantity;
-    }   
+        ProductStatus status;
+    }
+
+    // Product storage
+
+    mapping (address => Product[]) public shopper;
+
+    enum ProductStatus {
+        Cancelled,
+        Sold,
+        Listed
+    }
     
     // events
     event StorefrontCreate();
@@ -45,12 +60,23 @@ contract MarketPlace {
     
     modifier isStoreOwnerOnly(){ require(role[msg.sender] == storeOwnerRole); _;}
 
-    modifier isStorefrontOwner(Storefront storefront){ require(msg.sender == storefront.owner); _;}
+    modifier ownsStorefront(uint _storefrontId){ require(msg.sender == storefronts[_storefrontId].owner); _;}
     
     modifier isShopperOnly(){ require(role[msg.sender] == shopperRole); _;}
 
+    modifier productAvailable (uint _storefrontId, uint _productId, uint _quantity) { require(storefronts[_storefrontId].products[_productId].quantity > _quantity); _; }
+
+    modifier paidEnough (uint _storefrontId, uint _productId, uint _quantity) { require(msg.value > storefronts[_storefrontId].products[_productId].price * _quantity); _; }
+
     constructor() public {
         owner = msg.sender;
+        storefrontCount = 0;
+    }
+
+    // fallback
+
+    function() public payable {
+        revert();
     }
 
     // General Actions
@@ -59,47 +85,108 @@ contract MarketPlace {
     }
     
     // Owner Actions
-    function addAdmin(address adminAddress) public isOwnerOnly returns (bool success){
-        role[adminAddress] = adminRole;
+    function addAdmin(address _adminAddress) public isOwnerOnly returns (bool success){
+        role[_adminAddress] = adminRole;
         return true;
     }
 
-    function removeAdmin(address adminAddress) public isOwnerOnly returns (bool success){
-        role[adminAddress] = shopperRole;
+    function removeAdmin(address _adminAddress) public isOwnerOnly returns (bool success){
+        role[_adminAddress] = shopperRole;
         return true;
     }
 
     // Admin Actions
 
-    function addStoreOwner(address storeOwnerAddress) public isAdminOnly returns (bool success){
-        role[storeOwnerAddress] = storeOwnerRole;
+    function addStoreOwner(address _storeOwnerAddress) public isAdminOnly returns (bool success){
+        role[_storeOwnerAddress] = storeOwnerRole;
         return true;
 
     }
 
-    function removeStoreOwner(address storeOwnerAddress) public isAdminOnly returns (bool success){
-        role[storeOwnerAddress] = shopperRole;
+    function removeStoreOwner(address _storeOwnerAddress) public isAdminOnly returns (bool success){
+        role[_storeOwnerAddress] = shopperRole;
         return true;
 
     }
 
     // Store Owner Actions
 
-    function createStorefront(){}
+    function createStorefront(string storeName) isStoreOwnerOnly returns (bool success) {
+       storefronts.push(Storefront({
+            id: storefrontCount,
+            owner: msg.sender,
+            name: storeName,
+            balance: 0,
+            isActive: true,
+            products: new Product[](0)
+       }));
 
-    function removeStorefront(){}
+       storefrontCount += 1;
+       return true;
+    }
 
-    function withdrawStoreFunds(){}
+    function deactivateStorefront(uint _storefrontId) ownsStorefront(_storefrontId) returns (bool success) {
+        storefronts[_storefrontId].isActive = false;
+        return true;
+    }
 
-    function addProduct(){}
+    function modifyStorefrontName(uint _storefrontId, string _name) ownsStorefront(_storefrontId) returns (bool success) {
+        storefronts[_storefrontId].name = _name;
+        return true;
+    }
 
-    function modifyProduct(){}
+    function addProduct(uint _storefrontId, string _name, uint _price, uint _quantity) ownsStorefront(_storefrontId) returns (bool success) {
+        Product memory newProduct = Product({
+            storefrontId: _storefrontId,
+            productId: storefronts[_storefrontId].products.length,
+            name: _name,
+            price: _price,
+            quantity: _quantity,
+            status: ProductStatus.Listed
+        });
+        
+        storefronts[_storefrontId].products.push(newProduct);
+        return true;
+    }
 
-    function removeProduct(){}
+    function deactivateProduct(uint _storefrontId, uint _productId) ownsStorefront(_storefrontId) returns (bool success) {
+        storefronts[_storefrontId].products[_productId].status = ProductStatus.Cancelled;
+        return true;        
+    }
+
+    function withdrawStoreFunds(uint _storefrontId) ownsStorefront(_storefrontId) returns (bool success) {
+        Storefront storage store = storefronts[_storefrontId];
+        uint previousBalance = store.balance;
+        store.balance = 0;
+        if(!storefronts[_storefrontId].owner.send(store.balance)) {
+            store.balance = previousBalance;
+        }
+        
+        return true;        
+    }
 
     // Shopper Actions
 
-    function buyProduct(){}
+    function buyProduct(uint _storefrontId, uint _productId, uint _quantity) 
+        productAvailable(_storefrontId, _productId, _quantity) 
+        paidEnough(_storefrontId, _productId, _quantity)
+        public 
+        payable 
+        returns (bool success) 
+    {      
+        // Dededuct from seller
+        storefronts[_storefrontId].products[_productId].quantity -= _quantity;
+        storefronts[_storefrontId].balance += msg.value;
+
+        // Transfer product
+        Product memory p = storefronts[_storefrontId].products[_productId];
+        p.quantity = _quantity;
+        p.status = ProductStatus.Sold;
+
+        shopper[msg.sender].push(p);
+
+        return true;
+    }
 
 }
 
